052a7aa9792cc5c933c5a702901842b8a3c5fdf3
mikedettmercom <mike@mikedettmer.com>
2015-12-03 08:07:17 -0800

    completed day 3, using separate .js files for each step

---
+++ day3part2.js
@@ -0,0 +1,73 @@
+// day 3, part 2
+// global var 'input' should still be set from the day3part1.js file. if running stand-alone, copy the input var here.
+var newTown = [];
+
+var santa = {
+  x: 0,
+  y: 0
+};
+
+var roboSanta = {
+  x: 0,
+  y: 0
+};
+
+function house(x,y) {
+  this.x = x;
+  this.y = y;
+  this.presents = 1;
+}
+
+function checkForHouse(whichSanta) {
+  var found = 0;
+  newTown.forEach(function(houseToSearchFor){
+    if(houseToSearchFor.x === whichSanta.x && houseToSearchFor.y === whichSanta.y) {
+      found = houseToSearchFor;
+    }
+  });
+  givePresent(found, whichSanta);
+}
+
+function givePresent(found, whichSanta) {
+  if(!found) {
+    var newHouse = new house(whichSanta.x, whichSanta.y);
+    newTown.push(newHouse);
+  } else {
+    found.presents++;
+  }
+}
+
+function followDirectionsFromElf() {
+  input.forEach(function(instruction, index) {
+    var whichSanta;
+    if( (index % 2) == 0 ) {
+      whichSanta = santa;
+    } else { whichSanta = roboSanta; }
+    switch(instruction) {
+      case "^":
+        whichSanta.y++;
+        break;
+      case "v":
+        whichSanta.y--;
+        break;
+      case ">":
+        whichSanta.x++;
+        break;
+      case "<":
+        whichSanta.x--;
+        break;
+      default:
+        alert("you received an faulty input of "+input+"!");
+        return 0;
+    }
+    checkForHouse(whichSanta);
+  });
+  console.log(newTown.length);
+  window.outputEl.innerText += '\nday 3, part 1: '+newTown.length;
+}
+
+(function init() {
+  checkForHouse(santa);
+  checkForHouse(roboSanta);
+  followDirectionsFromElf();
+})();
